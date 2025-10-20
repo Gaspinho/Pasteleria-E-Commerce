7f871c34d843639e920416167bb987fe126cb61d
P0rotos <sebgarcia2021@inf.udec.cl>
2025-10-20 17:13:22 -0300

    delete, get, update user

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, Request
 from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel, Field
 from supabase import create_client, Client
+from datetime import datetime
 import os
+import json
 
 SUPABASE_URL = os.environ.get("SUPABASE_URL")
 SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
@@ -18,6 +20,23 @@ class TokenResponse(BaseModel):
     refresh_token: str
     user_id: str
 
+class UserDataGet(BaseModel):
+    email: str
+    phone: str
+    created_at: datetime
+    last_login_at: datetime | None
+    house_number: str
+    street_number: str
+    area: str
+    city: str
+
+class UserDataUpdate(BaseModel):
+    phone: str | None
+    house_number: str | None
+    street_number: str | None
+    area: str | None
+    city: str | None
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login") 
 
 async def get_access_token(token: str = Depends(oauth2_scheme)):
@@ -32,6 +51,7 @@ async def supa_register(user: User):
         metadata["admin"] = user.admin
     try:
         response = supabase.auth.admin.create_user({"email" : user.email, "password" : user.password, "data" : metadata})
+        print(response.user.id)
         _ = supabase.auth.admin.update_user_by_id( #bypass email confirmation
             response.user.id, 
             {"email_confirm": True}
@@ -64,7 +84,7 @@ async def supa_login(user: User):
             )
     except Exception as e:
         print(f"Supabase error: {e}")
-        raise HTTPException(status_code=400, detail="Supabase error during registration")
+        raise HTTPException(status_code=400, detail="Supabase error during login")
 
 @router.post("/logout")
 async def supa_logout(token: str = Depends(get_access_token)):
@@ -73,4 +93,56 @@ async def supa_logout(token: str = Depends(get_access_token)):
         return {"message": "User logged out successfully"}
     except Exception as e:
         print(f"Supabase error: {e}")
-        raise HTTPException(status_code=500, detail="Internal server error during registration.")
+        raise HTTPException(status_code=500, detail="Supabase error during logout")
+
+@router.post("/delete")
+async def supa_delete(token: str = Depends(get_access_token)):
+    try:
+        print("a")
+        _ = supabase.auth.admin.delete_user(token)
+        return {"message": "User deleted successfully"}
+    except Exception as e:
+        print(f"Supabase error: {e}")
+        raise HTTPException(status_code=401, detail="Supabase error during deletion")
+
+@router.get("/get_user", response_model=UserDataGet)
+async def supa_get_user(token: str = Depends(get_access_token)):
+    try:
+        response = supabase.auth.get_user(token)
+        return UserDataGet(
+            email=response.user.email,
+            phone=response.user.phone,
+            created_at=response.user.created_at,
+            last_login_at=response.user.last_sign_in_at,
+            house_number=response.user.user_metadata.get('house_number', ''),
+            street_number=response.user.user_metadata.get('street_number', ''),
+            area=response.user.user_metadata.get('area', ''),
+            city=response.user.user_metadata.get('city', '')
+        )
+    except Exception as e:
+        print(f"Supabase error: {e}")
+        raise HTTPException(status_code=500, detail="Supabase error during deletion")
+
+@router.post("/update_user")
+async def supa_update_user(userdata: UserDataUpdate, token: str = Depends(get_access_token), request: Request = None):
+    try:
+        refresh_token = request.headers.get("X-Refresh-Token")
+    except:
+        print(e)
+        raise HTTPException(status_code=500, detail="No Refresh token given")
+    try:
+        supabase.auth.set_session(access_token=token, refresh_token=refresh_token)
+        data_dict = userdata.model_dump(exclude_none=True)
+        phone_value = data_dict.pop('phone', None)
+        user_metadata = data_dict
+        payload = {}
+        if phone_value is not None:
+            payload['phone'] = phone_value
+        if user_metadata:
+            payload['data'] = user_metadata
+        print(payload)
+        _ = supabase.auth.update_user(payload)
+        return {"message": "User data updated successfully"}
+    except Exception as e:
+        print(f"Supabase error: {e}")
+        raise HTTPException(status_code=500, detail="Supabase error during deletion")
